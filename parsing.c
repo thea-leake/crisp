ac824e80c1bbeaeef96f3a27153585ea8a612a8b
Thea Leake <thea.leake@gmail.com>
2018-04-07 03:11:06 -0700

    Added decimal numbers

--- parsing.c
+++ parsing.c
@@ -23,7 +23,7 @@ int main(int argc, char** argv) {
 
     mpca_lang(MPCA_LANG_DEFAULT,
         "                                                        \
-            number:   /-?[0-9]+/ ;                               \
+            number: /-?[0-9]+\\.[0-9]+/ | /-?[0-9]+/  ;          \
             operator: '+' | '-' | '*' | '/' | '%' | /add/ |      \
                 /sub/ | /div/ | /mod/ ;                          \
             expr:     <number> | '('<operator> <expr>+ ')' ;     \
