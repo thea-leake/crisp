f40ab3e9dc0393a031e0dd09a8b97801688c312b
Thea Leake <thea.leake@gmail.com>
2018-04-07 00:11:43 -0700

    Removed windows crutch functions - not going to worry about windows..

--- prompt.c
+++ prompt.c
@@ -1,30 +1,15 @@
+/*
+ * Thea's Lisp
+ * BSD 3 License
+ * Implemented foloowing Build Your Own Lisp - http://www.buildyourownlisp.com/
+ * Thea Leake
+ * https://github.com/thea-leake/build_a_lisp
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <mpc/mpc.h>
-
-/* If we are compiling on Windows compile these functions */
-#ifdef _WIN32
-#include <string.h>
-
-static char buffer[2048];
-
-/* Fake readline function */
-char* readline(char* prompt) {
-  fputs(prompt, stdout);
-  fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
-  strcpy(cpy, buffer);
-  cpy[strlen(cpy)-1] = '\0';
-  return cpy;
-}
-
-/* Fake add_history function */
-void add_history(char* unused) {}
-
-/* Otherwise include the editline headers */
-#else
 #include <editline/readline.h>
-#endif
 
 int main(int argc, char** argv) {
     // Version and exit information
