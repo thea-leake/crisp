fd5a304b43c05d3418eed542c9064a392405d426
Thea Leake <thea.leake@gmail.com>
2018-04-24 02:04:01 -0700

    Added mod back in

--- operators.c
+++ operators.c
@@ -7,11 +7,11 @@ lval sum_op(lval v[], int expr_ct){
     int init_index = func_index + 1;
     int iter_start = init_index + 1;
     if (expr_ct == 1){
-        return v[0];
+        return v[func_index];
     }
     // return additive operator as it has no operands
-    lval init_val = v[1];
-    if (init_val.type == LVAL_ERR || expr_ct == 2){
+    lval init_val = v[init_index];
+    if (init_val.type == LVAL_ERR || expr_ct == iter_start){
         // return only operative, as there is nothing given to add to it
         return (init_val);
     }
@@ -46,13 +46,13 @@ lval sub_op(lval v[], int expr_ct){
     int init_index = func_index + 1;
     int iter_start = init_index + 1;
     if (expr_ct == 1){
-        return v[0];
+        return v[func_index];
     }
-    lval init_val = v[1];
+    lval init_val = v[init_index];
     if (init_val.type == LVAL_ERR ){
         return (init_val);
     }
-    if (expr_ct == 2) {
+    if (expr_ct == iter_start) {
         if (init_val.type == LVAL_NUM_INT) {
             int negated = init_val.num_int * -1;
             return lval_num_int(negated);
@@ -63,13 +63,10 @@ lval sub_op(lval v[], int expr_ct){
         }
     }
 
-    int is_int;
     float accum;
     if (init_val.type == LVAL_NUM_INT){
-        is_int = 0;
         accum = init_val.num_int;
     } else {
-        is_int = 1;
         accum = init_val.num_float;
     }
 
@@ -80,7 +77,6 @@ lval sub_op(lval v[], int expr_ct){
         }
         else  if (tmp.type == LVAL_NUM_FLOAT) {
             accum = accum - tmp.num_float;
-            is_int = 1;
         }
     }
     int accum_int = (int) accum;
@@ -98,10 +94,10 @@ lval mul_op(lval v[], int expr_ct){
     int init_index = func_index + 1;
     int iter_start = init_index + 1;
     if (expr_ct == 1){
-        return v[0];
+        return v[func_index];
     }
-    lval init_val = v[1];
-    if (init_val.type == LVAL_ERR || expr_ct == 2){
+    lval init_val = v[init_index];
+    if (init_val.type == LVAL_ERR || expr_ct == iter_start){
         return (init_val);
     }
 
@@ -135,9 +131,9 @@ lval div_op(lval v[], int expr_ct){
     int init_index = func_index + 1;
     int iter_start = init_index + 1;
     if (expr_ct == 1){
-        return v[0];
+        return v[func_index];
     }
-    lval init_val = v[1];
+    lval init_val = v[init_index];
     if (init_val.type == LVAL_ERR || expr_ct == 2){
         return (init_val);
     }
@@ -151,12 +147,18 @@ lval div_op(lval v[], int expr_ct){
 
     for (int i=iter_start; i < expr_ct; i++){
         lval tmp = v[i];
-        if (tmp.type == LVAL_NUM_INT){
-            accum =  accum / tmp.num_int;
-        }
-        else  if (tmp.type == LVAL_NUM_FLOAT) {
-            accum = accum / tmp.num_float;
-        }
+          if (tmp.type == LVAL_NUM_INT){
+              if (tmp.num_int == 0){
+                  return lval_err("Unable to divide by 0");
+              }
+              accum = accum / tmp.num_int;
+          }
+          else  if (tmp.type == LVAL_NUM_FLOAT) {
+              if (tmp.num_float == 0){
+                  return lval_err("Unable to divide by 0");
+              }
+              accum = accum / tmp.num_float;
+          }
     }
     int accum_int = (int) accum;
     if (accum == accum_int) {
@@ -164,4 +166,29 @@ lval div_op(lval v[], int expr_ct){
          return lval_num_int(s);
     }
     return lval_num_float(accum);
+}
+
+lval mod_op(lval v[], int expr_ct){
+    // printf("Starting sum\n");
+    int func_index = 0;
+    int init_index = func_index + 1;
+    int iter_start = init_index + 1;
+    if (expr_ct == 1){
+        return v[func_index];
+    }
+    lval init_val = v[init_index];
+    if (init_val.type == LVAL_ERR || expr_ct == iter_start){
+        return (init_val);
+    }
+
+    int accum = init_val.num_int;
+
+    for (int i=iter_start; i < expr_ct; i++){
+        lval tmp = v[i];
+        if (tmp.type != LVAL_NUM_INT) {
+            return lval_err("Non-int passed into modulo");
+        }
+        accum =  accum % tmp.num_int;
+    }
+    return lval_num_int(accum);
 }
