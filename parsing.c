49649489a4ba3581e3e0a6138d885b9a5a57c17e
Thea Leake <thea.leake@gmail.com>
2018-05-08 19:52:02 -0700

    Have quote somewhat working

--- parsing.c
+++ parsing.c
@@ -16,10 +16,15 @@
 lval* eval(mpc_ast_t* t);
 lval* eval_func(list * l);
 lval* get_eval_type(mpc_ast_t* t);
+lval* get_atom_type(mpc_ast_t* t);
+lval* get_literal(mpc_ast_t* t);
+lval* list_retrieve(mpc_ast_t* t, int is_literal);
+list* build_list(mpc_ast_t* t, int count, int accum_count, int is_literal);
+int get_literal_list_index(mpc_ast_t* t, int index);
 
 
-list* build_list(mpc_ast_t* t, int count, int accum_count){
-    lval* tmp = get_eval_type(t->children[accum_count]);
+list* build_list(mpc_ast_t* t, int count, int accum_count, int is_literal){
+    lval* tmp = list_retrieve(t->children[accum_count], is_literal);
     int next_count = accum_count + 1;
     if (next_count == count) {
         if (tmp->type != LVAL_NOOP){
@@ -29,14 +34,57 @@ list* build_list(mpc_ast_t* t, int count, int accum_count){
         return NULL;
     }
     if (tmp->type != LVAL_NOOP){
-        list* next_elem =  build_list(t, count, next_count);
+        list* next_elem =  build_list(t, count, next_count, is_literal);
         return prepend_create(tmp, next_elem);
     }
     lval_del(tmp);
-    return build_list(t, count, next_count);
+    return build_list(t, count, next_count, is_literal);
+}
+
+lval* list_retrieve(mpc_ast_t* t, int is_literal){
+   if (is_literal == 1){
+      return get_literal(t);
+   }
+   return get_eval_type(t);
 }
 
 lval* get_eval_type(mpc_ast_t* t){
+    if (strstr(t->tag, "literal")){
+        int index = get_literal_list_index(t, 0);
+        list* l = build_list(t, t->children_num, index, 1);
+        lval* v = lval_list(l);
+        return v;
+    }
+    if (strstr(t->tag, "list")){
+        list* l = build_list(t, t->children_num, 0, 0);
+        lval* v = eval_func(l);
+        return v;
+    }
+    if (strstr(t->tag, "nil")){
+        lval* v = lval_nil();
+        return v;
+    }
+   lval* v = get_atom_type(t);
+    return v;
+}
+
+
+lval* get_literal(mpc_ast_t* t){
+    if ((strstr(t->tag, "literal")) || (strstr(t->tag, "list"))){
+        int index = get_literal_list_index(t, 0);
+        list* l = build_list(t, t->children_num, index, 1);
+        lval* v = lval_list(l);
+        return v;
+    }
+    if (strstr(t->tag, "nil")){
+        lval* v = lval_nil();
+        return v;
+    }
+   lval* v = get_eval_type(t);
+    return v;
+}
+
+lval* get_atom_type(mpc_ast_t* t){
     if (strstr(t->tag, "string")){
         lval* v = lval_str(t->contents);
         return v;
@@ -53,20 +101,6 @@ lval* get_eval_type(mpc_ast_t* t){
         lval* v = lval_func(t->contents);
         return v;
     }
-    if (strstr(t->tag, "list")){
-        list* l = build_list(t, t->children_num, 0);
-        lval* v = eval_func(l);
-        return v;
-    }
-    if (strstr(t->tag, "literal")){
-        list* l = build_list(t, t->children_num, 0);
-        lval* v = lval_list(l);
-        return v;
-    }
-    if (strstr(t->tag, "nil")){
-        lval* v = lval_nil();
-        return v;
-    }
    lval* v = lval_noop();
     return v;
 }
@@ -78,6 +112,16 @@ int get_ast_expr_index(mpc_ast_t* t, int index){
     }
     int n = index + 1;
     return get_ast_expr_index(t, n);
+}
+
+int get_literal_list_index(mpc_ast_t* t, int index){
+    mpc_ast_t* ti = t->children[index];
+    if (strstr(ti->tag, "atom") || (strstr(ti->tag, "list"))){
+          return index;
+    }
+    int n = index + 1;
+    return get_literal_list_index(t, n);
+
 }
 
 lval* eval(mpc_ast_t* t){
@@ -159,7 +203,7 @@ int main(int argc, char** argv) {
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)){
             // print AST
-            //mpc_ast_print(r.output);
+            mpc_ast_print(r.output);
             lval* result = eval(r.output);
             print_lval(result);
             printf("\n");
