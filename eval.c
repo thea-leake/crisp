a5b54c3940bcea3de0904c280f31b7774267f158
Thea Leake <thea.leake@gmail.com>
2018-06-02 02:45:21 -0700

    Added start of lambda functionality code

--- eval.c
+++ eval.c
@@ -32,5 +32,35 @@ lval* eval_lval(env* e, lval* v){
 lval* eval_func(env* e, list * l){
     lval* func_lval = eval_lval(e, l->expr);
     list* operands = l->next;
-    return (func_lval->func->func)(e, operands);
+    // if builtin
+    if (func_lval->type == LVAL_FUNC){
+       return (func_lval->func->func)(e, operands);
+    }
+    // create fn env, with parent env to search for matching symbols if no matches
+    // found in lambda vars
+    env* fn_vars = init_env(e);
+    lval* env_set = build_scoped_env(e, func_lval->lambda->var_expr, operands);
+    if (env_set->type == LVAL_ERR){
+       del_env(fn_vars);
+       return env_set;
+    }
+    lval_del(env_set);
+    lval* lambda_eval = eval_func(fn_vars, func_lval->lambda->eval_expr);
+    // del will cascade through all lambda vars, but not parent env.
+    del_env(fn_vars);
+    return lambda_eval;
+}
+
+lval* build_scoped_env(env* e, list* vars, list* vals){
+   if (vars == NULL){
+      if (vals != NULL){
+         return lval_err("Too many arguments for procedure");
+      }
+      return lval_nil();
+   }
+   if (vals == NULL){
+      return lval_err("Not enough arguments for procedure");
+   }
+   put_val(e, vals->expr, vars->expr->sym);
+   return build_scoped_env(e, vars->next, vals->next);
 }
--- eval.h
+++ eval.h
@@ -7,5 +7,8 @@
 list* eval_list(env* e, list* l, bool list_start);
 lval* eval_lval(env* e, lval* l);
 lval* eval_func(env* e, list * l);
+lval* eval_lambda(env* e, lval v);
+
+lval* build_scoped_env(env* e, list* vars, list* vals);
 
 #endif
